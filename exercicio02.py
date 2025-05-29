7c7b91af6587ab03566d78b82145e3e1b92ef1cf
Lara Braz <lara.braz@escola.pr.gov.br>
2025-05-29 14:28:39 +0000

    exercíicio quase finalizado

--- exercicio02.py
+++ exercicio02.py
@@ -3,3 +3,17 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
+tarefaX = int(input("Digite o tempo necessário para terminar a tarefa X: "))
+tarefaY = int(input("Digite o tempo necessário para terminar a tarefa Y: "))
+tarefaZ = int(input("Digite o tempo necessário para terminar a tarefa Z: "))
+
+if tarefaX + tarefaY + tarefaZ =< 0
+    print("O tempo determinado para finalizar as tarefas é de: ")
+
+
+
+
+
+
+
+
