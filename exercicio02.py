b83008fb795165aebcba164e402a9d386c5f5f9c
Lara Braz <lara.braz@escola.pr.gov.br>
2025-06-02 14:03:17 +0000

    exercício finalizado

--- exercicio02.py
+++ exercicio02.py
@@ -3,14 +3,15 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
-tarefaX = int(input("Digite o tempo necessário para terminar a tarefa X: "))
-tarefaY = int(input("Digite o tempo necessário para terminar a tarefa Y: "))
-tarefaZ = int(input("Digite o tempo necessário para terminar a tarefa Z: "))
+dia_01 = int(input("Digite o tempo necessário para terminar a tarefa X: "))
+dia_02 = int(input("Digite o tempo necessário para terminar a tarefa Y: "))
+dia_03 = int(input("Digite o tempo necessário para terminar a tarefa Z: "))
+soma = dia_03 + dia_02 + dia_03
 
-if tarefaX + tarefaY + tarefaZ =< 0
-    print("O tempo determinado para finalizar as tarefas é de: ")
+if dia_01 < dia_02 < 0 or dia_03 < 0:
+    print("Erro: tempo determinado para a conclusão das tarefas acabou. ")
 else:
-    print("")
+    print(f"Tempo total do projeto: {soma} dias")
 
 
 
