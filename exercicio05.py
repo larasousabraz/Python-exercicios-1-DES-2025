202c50e20f88e10a08031b7af9f63be806590e9c
Lara Braz <lara.braz@escola.pr.gov.br>
2025-06-05 14:36:44 +0000

    exercício finalizado

--- exercicio05.py
+++ exercicio05.py
@@ -1,3 +1,8 @@
 # Diego está acompanhando seu consumo de internet mensal, que tem um limite de 100 GB.
 # O programa deve receber o total consumido e avisar se ele ultrapassou o limite.
 
+internet = int(input("Digite o consumo de internet atual: "))
+if internet > 100:
+    print("O limite de consumo de internet mensal foi ultrapassado.")
+else:
+    print(f"O limite de consumo tem um armazenamento de {internet}.")
