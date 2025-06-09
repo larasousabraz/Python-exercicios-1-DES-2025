786a15cff241ee17d008775e3450e6fa646f5625
Lara Braz <lara.braz@escola.pr.gov.br>
2025-06-09 14:00:56 +0000

    Lista de frutas finalizada.

--- exercicio05.py
+++ exercicio05.py
@@ -3,6 +3,6 @@
 
 internet = int(input("Digite o consumo de internet atual: "))
 if internet > 100:
-    print("O limite de consumo de internet mensal foi ultrapassado.")
+    print(f"O limite de consumo de internet mensal ({internet}) foi ultrapassado.")
 else:
     print(f"O limite de consumo tem um armazenamento de {internet}.")
--- exercicio06.py
+++ exercicio06.py
@@ -1,3 +1,6 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
+horario = int(input("Digite o horário atual (24h): "))
+if horario < 9 and > 21:
+    print("A plata")
