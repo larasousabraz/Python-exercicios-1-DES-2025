54d25685b32e9d00617c74f1e53670008141368c
Lara Braz <lara.braz@escola.pr.gov.br>
2025-06-23 13:54:38 +0000

    feito

--- exercicio010.py
+++ exercicio010.py
@@ -4,9 +4,7 @@
 # A parcela não pode ser maior que 35% do salário
 
 salario = float(input("Digite o salário mensal: "))
-parcela = int(input("Digite o percentual que o financiamento ocupa no salário: "))
+parcela = int(input("Digite o percentual que i financiamento ocupa no salário: "))
 
-if  salario > 3000 or parcela > 35:
-    print("Infelizmente, o financiamento não pode ser realizado.")
-else:
-    print("O financiamento pode ser realizado.")
+if  salario > 3000 or parcela < 35:
+    
