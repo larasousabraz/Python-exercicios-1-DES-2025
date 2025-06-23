a1054c65c6ac8d8f24f41e29621fb9e7b0c99b04
Lara Braz <lara.braz@escola.pr.gov.br>
2025-06-23 18:54:41 +0000

    feito

--- exercicio12.py
+++ exercicio12.py
@@ -2,4 +2,11 @@
 ##Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.  
 ##Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
 
+senha = input("Digite uma senha: ")
 
+if senha == "":
+    print("Senha muito curta")
+elif len(senha) >= 8: 
+    print("Senha válida")
+else:
+    print("Senha muito curta")
